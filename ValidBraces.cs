cc895a6db151a49bcef731dfc5f1958a534bc9cc
Virtical <138823554+Virtical@users.noreply.github.com>
2024-08-28 13:06:14 +0500

    Add ValidBraces.cs

---
+++ ValidBraces.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Brace
+{
+    public static bool validBraces(String braces)
+    {
+        var bracesStack = new Stack<char>();
+
+        foreach (var brace in braces)
+        {
+            if (brace == '(' || brace == '[' || brace == '{')
+            {
+                bracesStack.Push(brace);
+            }
+            else
+            {
+                if (bracesStack.Count != 0 && ((brace == ')' && bracesStack.Peek() == '(') || (brace == ']' && bracesStack.Peek() == '[') || (brace == '}' && bracesStack.Peek() == '{')))
+                {
+                    bracesStack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (bracesStack.Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
