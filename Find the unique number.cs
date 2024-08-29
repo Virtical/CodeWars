cadba381c9cd40e5113d93b762615ac0e2c1db66
Virtical <138823554+Virtical@users.noreply.github.com>
2024-08-29 09:07:32 +0500

    Add Find the unique number.cs

---
+++ Find the unique number.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int GetUnique(IEnumerable<int> numbers)
+    {
+        var firstList = new List<int>();
+        var secondList = new List<int>();
+
+        foreach (var number in numbers)
+        {
+            if (firstList.Count == 0)
+            {
+                firstList.Add(number);
+            }
+            else if (number == firstList.First())
+            {
+                firstList.Add(number);
+            }
+            else
+            {
+                secondList.Add(number);
+            }
+        }
+
+        return firstList.Count > 1 ? secondList.First() : firstList.First();
+    }
+}
