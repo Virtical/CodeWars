842983b488ab6677663da52d408b0ac03ff8b503
Virtical <138823554+Virtical@users.noreply.github.com>
2024-08-29 09:49:31 +0500

    Add Which are in.cs

---
+++ Which are in.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class WhichAreIn
+{
+    public static string[] inArray(string[] array1, string[] array2)
+    {
+        var d = new HashSet<string>();
+        var r = array1.Where(x => array2.Where(y => y.Contains(x)).Count() > 0).ToList();
+        r.Sort();
+        return r.ToArray();
+    }
+}
