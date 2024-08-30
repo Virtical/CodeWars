b99673b4285d581e3fba5d9d7a3571b173eb7a5a
Virtical <138823554+Virtical@users.noreply.github.com>
2024-08-30 11:53:45 +0500

    Add Write Number in Expanded Form.cs

---
+++ Write Number in Expanded Form.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+
+public static class Kata
+{
+    public static string ExpandedForm(long num)
+    {
+        var str = num.ToString();
+        var strBuilder = new StringBuilder();
+
+        for (var i = 0; i < str.Length; i++) 
+        {
+            if (str[i] != '0')
+            {
+                strBuilder.Append(str[i]);
+                for (var j = str.Length - 1; j > i; j--)
+                {
+                    strBuilder.Append('0');
+                }
+                strBuilder.Append(" + ");
+            }
+        }
+
+        strBuilder.Remove(strBuilder.Length - 3, 3);
+
+        return strBuilder.ToString();
+    }
+}
