4001fcb35b375105edfd4f122eab028e433ea126
Virtical <138823554+Virtical@users.noreply.github.com>
2024-09-09 21:48:26 +0500

    Add Replace With Alphabet Position.cs

---
+++ Replace With Alphabet Position.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+public static class Kata
+{
+    public static string AlphabetPosition(string text)
+    {
+        return string.Join(" ", text.Where(char.IsLetter).Select(x => (byte)char.ToLower(x) - 96).ToList());
+    }
+}
